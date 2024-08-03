37ade23ba54d6018ab1a7b70e2a9a95eb5deb8f3
Joao Coelho <joaonc@hotmail.com>
2024-08-03 11:31:04 -0500

    refactor package functions

--- src/pytest_params/params.py
+++ src/pytest_params/params.py
@@ -1,3 +1,3 @@
 def params():
     """Temporary stub."""
-    return 'test'
+    return 'test1'
---
+++ tests/test_request_params.py
@@ -0,0 +1,5 @@
+from src.pytest_params import get_request_param
+
+
+def test_get_request_param():
+    assert get_request_param() == 'test2'
