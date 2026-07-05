ea1b21283583bfe2542c44971b2d16f3ba547083
Joao Coelho <joaonc@hotmail.com>
2026-07-05 17:28:17 -0500

    import fix

--- src/pytest_params/params.pyi
+++ src/pytest_params/params.pyi
@@ -3,7 +3,7 @@ from typing import Any, Iterable, Literal, Sequence, Union
 from _pytest.mark.structures import MarkDecorator, ParameterSet
 
 # Define _ScopeName type for both pytest 7 and 8 compatibility
-_ScopeName = Union[
+ScopeName = Union[
     Literal['session'], Literal['package'], Literal['module'], Literal['class'], Literal['function']
 ]
 
@@ -12,6 +12,6 @@ def params(
     name_values: Iterable[tuple],  # Iterable[tuple[str, ...]]
     *,
     indirect: Union[bool, Sequence[str]] = False,
-    scope: Union[_ScopeName, None] = None,
+    scope: Union[ScopeName, None] = None,
 ) -> MarkDecorator: ...
 def params_values(*name_values: Any) -> list[ParameterSet]: ...
